19c7e6df8785e0b99705fc530fb27ab1feeceb6a
Rachit Garg <rachitgarg900@gmail.com>
2022-07-27 13:04:13 +0200

    Added Macro based Makefile

---
+++ With_Macros/include/hello.h
@@ -0,0 +1,11 @@
+// Guard string to prevent multiple inclusion
+#ifndef __HELLO__
+#define __HELLO__
+
+void say_hello();
+
+static const char* anystring = "RSTAR900 :)" ;
+
+typedef int dType;
+
+#endif
---
+++ With_Macros/src/hello.cpp
@@ -0,0 +1,10 @@
+#include "hello.h"
+#include <iostream>
+using namespace std;
+
+dType x = 32;
+
+void say_hello()
+{
+	cout << "Hello from " << anystring << " " << x << endl; 
+}
